8b67c1884351d9f00453b52d409182bdd9abe821
xingxiaolin <xing.xiaolin@foxmail.com>
2025-06-20 23:42:19 +0800

    [docs]更新README

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: () => import("@/views/HomeView.vue"),
-      meta: { hideFooter: true,  hideSidebar: true } 
+      meta: { hideFooter: true,  hideSidebar: false }
     },
     {
       path: "/popular",
@@ -19,7 +19,7 @@ const router = createRouter({
       path: "/video",
       name: "video",
       component: () => import("@/views/pages/VideoView.vue"),
-      meta: { hideFooter: true, hideSidebar: true} 
+      meta: { hideFooter: true, hideSidebar: false}
     },
     {
       path: '/login',
